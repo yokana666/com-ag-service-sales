dbe32499b64f4f7930939931f9fc46cc8032d580
kharissuryandaru <35208779+kharissuryandaru@users.noreply.github.com>
2018-08-29 14:03:23 +0700

    Fixed WeavingSalesContractReport

---
+++ Com.Danliris.Service.Sales.WebApi/Controllers/WeavingSalesContractReportController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades;
+using Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades.Weaving;
+using Com.Danliris.Service.Sales.WebApi.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Com.Danliris.Service.Sales.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/sales/reports/weaving-sales-contract-report")]
+    [Authorize]
+    public class WeavingSalesContractReportController : Controller
+    {
+        private string ApiVersion = "1.0.0";
+        private readonly WeavingSalesContractFacade weavingSalesContractFacade;
+
+        public WeavingSalesContractReportController(WeavingSalesContractFacade weavingSalesContractFacade)
+        {
+            this.weavingSalesContractFacade = weavingSalesContractFacade;
+        }
+    }
+}
