7f3b24c68e3b39598b605997909d29538f08273c
achensetyawati <achenzetya@gmail.com>
2018-09-28 14:35:29 +0700

    article color migration

---
+++ Com.Danliris.Service.Sales.Lib/Migrations/20180927083601_AddArticleColor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Sales.Lib.Migrations
+{
+    public partial class AddArticleColor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ArticleColors",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Active = table.Column<bool>(nullable: false),
+                    CreatedUtc = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 255, nullable: false),
+                    CreatedAgent = table.Column<string>(maxLength: 255, nullable: false),
+                    LastModifiedUtc = table.Column<DateTime>(nullable: false),
+                    LastModifiedBy = table.Column<string>(maxLength: 255, nullable: false),
+                    LastModifiedAgent = table.Column<string>(maxLength: 255, nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedUtc = table.Column<DateTime>(nullable: false),
+                    DeletedBy = table.Column<string>(maxLength: 255, nullable: false),
+                    DeletedAgent = table.Column<string>(maxLength: 255, nullable: false),
+                    UId = table.Column<string>(maxLength: 255, nullable: true),
+                    Name = table.Column<string>(maxLength: 255, nullable: true),
+                    Description = table.Column<string>(maxLength: 4000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticleColors", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArticleColors");
+        }
+    }
+}
