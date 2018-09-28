eda6503a6b41dcbabaea04b9e696d8c65b3b6c71
achensetyawati <achenzetya@gmail.com>
2018-09-28 14:36:31 +0700

    RO_Garment

---
+++ Com.Danliris.Service.Sales.Lib/BusinessLogic/Facades/ROGarment/ROGarmentFacade.cs
@@ -0,0 +1,116 @@
+﻿using Com.Danliris.Service.Sales.Lib.BusinessLogic.Interface.ROGarmentInterface;
+using Com.Danliris.Service.Sales.Lib.BusinessLogic.Logic.ROGarmentLogics;
+using Com.Danliris.Service.Sales.Lib.Models.ROGarments;
+using Com.Danliris.Service.Sales.Lib.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Text;
+using Com.Danliris.Service.Sales.Lib.Helpers;
+using System.Threading.Tasks;
+using System.Linq;
+using Com.Danliris.Service.Sales.Lib.Utilities;
+using Com.Danliris.Service.Sales.Lib.Models.CostCalculationGarments;
+using Com.Danliris.Service.Sales.Lib.BusinessLogic.Logic.CostCalculationGarments;
+using Com.Danliris.Service.Sales.Lib.BusinessLogic.Interface.CostCalculationGarmentLogic;
+
+namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades.ROGarment
+{
+    public class ROGarmentFacade : IROGarment
+    {
+        private readonly SalesDbContext DbContext;
+        private readonly DbSet<RO_Garment> DbSet;
+        private readonly IdentityService identityService;
+        private readonly ROGarmentLogic roGarmentLogic;
+        private readonly ICostCalculationGarment costCalGarmentLogic;
+        public IServiceProvider ServiceProvider;
+
+        public ROGarmentFacade(IServiceProvider serviceProvider, SalesDbContext dbContext)
+        {
+            DbContext = dbContext;
+            DbSet = DbContext.Set<RO_Garment>();
+            identityService = serviceProvider.GetService<IdentityService>();
+            roGarmentLogic = serviceProvider.GetService<ROGarmentLogic>();
+            costCalGarmentLogic = serviceProvider.GetService<ICostCalculationGarment>();
+            ServiceProvider = serviceProvider;
+        }
+        private AzureImageFacade AzureImageFacade
+        {
+            get { return this.ServiceProvider.GetService<AzureImageFacade>(); }
+        }
+
+        public async Task<int> CreateAsync(RO_Garment Model)
+        {
+            do
+            {
+                Model.Code = Code.Generate();
+            }
+            while (this.DbSet.Any(d => d.Code.Equals(Model.Code)));
+
+            CostCalculationGarment costCalculationGarment = Model.CostCalculationGarment;
+            Model.CostCalculationGarment = null;
+
+            Model.ImagesPath = await this.AzureImageFacade.UploadMultipleImage(Model.GetType().Name, (int)Model.Id, Model.CreatedUtc, Model.ImagesFile, Model.ImagesPath);
+            roGarmentLogic.Create(Model);
+            await DbContext.SaveChangesAsync();
+            //Model.ImagesPath = await this.AzureImageService.UploadMultipleImage(Model.GetType().Name, Model.Id, Model._CreatedUtc, Model.ImagesFile, Model.ImagesPath);
+
+            //await this.UpdateAsync((int)Model.Id, Model);
+            //update CostCal
+
+            return await UpdateCostCalAsync(costCalculationGarment, (int)Model.Id);
+        }
+
+        public async Task<int> UpdateCostCalAsync(CostCalculationGarment costCalculationGarment, int Id)
+        {
+            costCalculationGarment.RO_GarmentId = Id;
+            await costCalGarmentLogic.UpdateAsync((int)costCalculationGarment.Id, costCalculationGarment);
+
+            return await DbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            await roGarmentLogic.DeleteAsync(id);
+            return await DbContext.SaveChangesAsync();
+        }
+
+        public ReadResponse<RO_Garment> Read(int page, int size, string order, List<string> select, string keyword, string filter)
+        {
+            return roGarmentLogic.Read(page, size, order, select, keyword, filter);
+        }
+
+        public async Task<RO_Garment> ReadByIdAsync(int id)
+        {
+            RO_Garment read = await this.DbSet
+                .Where(d => d.Id.Equals(id) && d.IsDeleted.Equals(false))
+                .Include(d => d.RO_Garment_SizeBreakdowns)
+                    .ThenInclude(sb => sb.RO_Garment_SizeBreakdown_Details)
+                .Include(d => d.CostCalculationGarment)
+                    .ThenInclude(ccg => ccg.CostCalculationGarment_Materials)
+                .FirstOrDefaultAsync();
+
+            read.CostCalculationGarment.ImageFile = await this.AzureImageFacade.DownloadImage(read.CostCalculationGarment.GetType().Name, read.CostCalculationGarment.ImagePath);
+            read.ImagesFile = await this.AzureImageFacade.DownloadMultipleImages(read.GetType().Name, read.ImagesPath);
+
+            return read;
+        }
+
+        public async Task<int> UpdateAsync(int id, RO_Garment Model)
+        {
+            CostCalculationGarment costCalculationGarment = Model.CostCalculationGarment;
+            Model.CostCalculationGarment = null;
+
+            Model.ImagesPath = await this.AzureImageFacade.UploadMultipleImage(Model.GetType().Name, (int)Model.Id, Model.CreatedUtc, Model.ImagesFile, Model.ImagesPath);
+
+            int updated = await this.UpdateAsync(id, Model);
+
+            costCalculationGarment.RO_GarmentId = (int)Model.Id;
+            await this.costCalGarmentLogic.UpdateAsync((int)costCalculationGarment.Id, costCalculationGarment);
+
+            return updated;
+        }
+
+    }
+}
---
+++ Com.Danliris.Service.Sales.Lib/BusinessLogic/Logic/ROGarmentLogics/ROGarmentLogic.cs
@@ -0,0 +1,118 @@
+﻿using Com.Danliris.Service.Sales.Lib.Models.ROGarments;
+using Com.Danliris.Service.Sales.Lib.Services;
+using Com.Danliris.Service.Sales.Lib.Utilities.BaseClass;
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+using Com.Danliris.Service.Sales.Lib.Utilities;
+using System.Linq;
+using Newtonsoft.Json;
+using Com.Moonlay.NetCore.Lib;
+using Com.Danliris.Service.Sales.Lib.Helpers;
+using Com.Moonlay.Models;
+
+namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Logic.ROGarmentLogics
+{
+    public class ROGarmentLogic : BaseLogic<RO_Garment>
+    {
+        private ROGarmentSizeBreakdownLogic roGarmentSizeBreakdownLogic;
+        private ROGarmentSizeBreakdownDetailLogic roGarmentSizeBreakdownDetailLogic;
+
+        private readonly SalesDbContext DbContext;
+        public ROGarmentLogic(IServiceProvider serviceProvider, IIdentityService identityService, SalesDbContext dbContext) : base(identityService, serviceProvider, dbContext)
+        {
+            this.roGarmentSizeBreakdownLogic = serviceProvider.GetService<ROGarmentSizeBreakdownLogic>();
+            this.roGarmentSizeBreakdownDetailLogic = serviceProvider.GetService<ROGarmentSizeBreakdownDetailLogic>();
+            this.DbContext = dbContext;
+        }
+
+        public override ReadResponse<RO_Garment> Read(int page, int size, string order, List<string> select, string keyword, string filter)
+        {
+            IQueryable<RO_Garment> Query = DbSet;
+
+            List<string> SearchAttributes = new List<string>()
+            {
+                "Code"
+            };
+
+            Query = QueryHelper<RO_Garment>.Search(Query, SearchAttributes, keyword);
+
+            Dictionary<string, object> FilterDictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(filter);
+            Query = QueryHelper<RO_Garment>.Filter(Query, FilterDictionary);
+
+            List<string> SelectedFields = new List<string>()
+            {
+                  "Id", "Code", "CostCalculationGarment", "Total"
+            };
+
+            Query = Query
+                 .Select(ccg => new RO_Garment
+                 {
+                     Id = ccg.Id,
+                     Code = ccg.Code,
+                     CostCalculationGarment=ccg.CostCalculationGarment,
+                     CostCalculationGarmentId=ccg.CostCalculationGarmentId,
+                     LastModifiedUtc = ccg.LastModifiedUtc
+                 });
+
+            Dictionary<string, string> OrderDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(order);
+            Query = QueryHelper<RO_Garment>.Order(Query, OrderDictionary);
+
+            Pageable<RO_Garment> pageable = new Pageable<RO_Garment>(Query, page - 1, size);
+            List<RO_Garment> data = pageable.Data.ToList<RO_Garment>();
+            int totalData = pageable.TotalCount;
+
+            return new ReadResponse<RO_Garment>(data, totalData, OrderDictionary, SelectedFields);
+        }
+
+
+        public override void Create(RO_Garment model)
+        {
+            do
+            {
+                model.Code = Code.Generate();
+            }
+            while (this.DbSet.Any(d => d.Code.Equals(model.Code)));
+
+            foreach (var size in model.RO_Garment_SizeBreakdowns)
+            {
+                roGarmentSizeBreakdownLogic.Create(size);
+            }
+
+            EntityExtension.FlagForCreate(model, IdentityService.Username, "sales-service");
+            DbSet.Add(model);
+        }
+
+        public override async void UpdateAsync(int id, RO_Garment model)
+        {
+            if (model.RO_Garment_SizeBreakdowns != null)
+            {
+                HashSet<long> detailIds = roGarmentSizeBreakdownLogic.GetIds(id);
+                foreach (var itemId in detailIds)
+                {
+                    RO_Garment_SizeBreakdown data = model.RO_Garment_SizeBreakdowns.FirstOrDefault(prop => prop.Id.Equals(itemId));
+                    if (data == null)
+                    {
+                        await roGarmentSizeBreakdownLogic.DeleteAsync(Convert.ToInt32(itemId));
+
+                    }
+                    else
+                    {
+                        roGarmentSizeBreakdownLogic.UpdateAsync(Convert.ToInt32(itemId), data);
+                    }
+
+                    foreach (RO_Garment_SizeBreakdown item in model.RO_Garment_SizeBreakdowns)
+                    {
+                        if (item.Id == 0)
+                            roGarmentSizeBreakdownLogic.Create(item);
+                    }
+                }
+            }
+
+            EntityExtension.FlagForUpdate(model, IdentityService.Username, "sales-service");
+            DbSet.Update(model);
+        }
+
+    }
+}
