3c02a5aab1dda17e225bf289f568962618877da5
Wahyu Widiarto <widiartowahyu3@gmail.com>
2019-02-15 16:16:06 +0700

    Revert "Revert "updated conflict merge""
    This reverts commit 841704dce0b4fb792054171d239c60b2d89408ad.

---
+++ Com.Danliris.Service.Sales.Lib/BusinessLogic/Interface/GarmentMasterPlan/WeeklyPlanInterfaces/IWeeklyPlanFacade.cs
@@ -0,0 +1,12 @@
+﻿using Com.Danliris.Service.Sales.Lib.Models.GarmentMasterPlan.WeeklyPlanModels;
+using Com.Danliris.Service.Sales.Lib.Utilities.BaseInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Interface.Garment.WeeklyPlanInterfaces
+{
+    public interface IWeeklyPlanFacade : IBaseFacade<WeeklyPlan>
+    {
+    }
+}
