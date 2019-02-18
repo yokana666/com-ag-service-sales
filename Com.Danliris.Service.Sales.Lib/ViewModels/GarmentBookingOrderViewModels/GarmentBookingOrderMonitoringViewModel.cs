6838e7b1e84024722137d4bc119c0694d45d048e
Wahyu Widiarto <widiartowahyu3@gmail.com>
2019-02-18 14:35:17 +0700

    fixx

--- Com.Danliris.Service.Sales.Lib/ViewModels/GarmentBookingOrderViewModels/GarmentBookingOrderMonitoringViewModel.cs
+++ Com.Danliris.Service.Sales.Lib/ViewModels/GarmentBookingOrderViewModels/GarmentBookingOrderMonitoringViewModel.cs
@@ -7,15 +7,15 @@ namespace Com.Danliris.Service.Sales.Lib.ViewModels.GarmentBookingOrderViewModel
 {
     public class GarmentBookingOrderMonitoringViewModel : BaseViewModel
     {
-        public string BookingOrderNo { get; set; }
-        public DateTimeOffset BookingOrderDate { get; set; }
-        public string BuyerName { get; set; }
-        public double OrderQuantity { get; set; }
-        public DateTimeOffset DeliveryDate { get; set; }
-        public string ComodityName { get; set; }
-        public double ConfirmQuantity { get; set; }
-        public DateTimeOffset DeliveryDateItems { get; set; }
-        public DateTimeOffset ConfirmDate { get; set; }
-        public string Remark { get; set; }
+        //public string BookingOrderNo { get; set; }
+        //public DateTimeOffset BookingOrderDate { get; set; }
+        //public string BuyerName { get; set; }
+        //public double OrderQuantity { get; set; }
+        //public DateTimeOffset DeliveryDate { get; set; }
+        //public string ComodityName { get; set; }
+        //public double ConfirmQuantity { get; set; }
+        //public DateTimeOffset DeliveryDateItems { get; set; }
+        //public DateTimeOffset ConfirmDate { get; set; }
+        //public string Remark { get; set; }
     }
 }
