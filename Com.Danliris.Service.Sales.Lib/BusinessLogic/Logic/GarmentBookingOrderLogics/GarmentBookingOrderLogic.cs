21da7af25e431c7591c0871ae82954eca5c5c0e5
kharissuryandaru <35208779+kharissuryandaru@users.noreply.github.com>
2019-03-29 09:53:01 +0700

    Update GarmentBookingOrderLogic.cs

--- Com.Danliris.Service.Sales.Lib/BusinessLogic/Logic/GarmentBookingOrderLogics/GarmentBookingOrderLogic.cs
+++ Com.Danliris.Service.Sales.Lib/BusinessLogic/Logic/GarmentBookingOrderLogics/GarmentBookingOrderLogic.cs
@@ -132,6 +132,12 @@ namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Logic.GarmentBookingOrder
         {
             var model = await DbSet.Include(d => d.Items).FirstOrDefaultAsync(d => d.Id == id);
             EntityExtension.FlagForDelete(model, IdentityService.Username, "sales-service", true);
+
+            if (model.IsBlockingPlan == true)
+            {
+                var blockingPlan = DbContext.GarmentSewingBlockingPlans.FirstOrDefault(b => b.BookingOrderId == model.Id);
+                blockingPlan.Status = "Booking Dihapus";
+            }
             
             foreach (var item in model.Items)
             {
