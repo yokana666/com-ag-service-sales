670f7f1703fc876ccd07ca52c0dd1a3bd325ee04
kharissuryandaru <35208779+kharissuryandaru@users.noreply.github.com>
2019-04-12 14:10:59 +0700

    Update AcceptedOrderMonitoringFacade.cs

--- Com.Danliris.Service.Sales.Lib/BusinessLogic/Facades/GarmentMasterPlan/MonitoringFacades/AcceptedOrderMonitoringFacade.cs
+++ Com.Danliris.Service.Sales.Lib/BusinessLogic/Facades/GarmentMasterPlan/MonitoringFacades/AcceptedOrderMonitoringFacade.cs
@@ -13,6 +13,9 @@ using System.Linq;
 using System.Data;
 using OfficeOpenXml;
 using Newtonsoft.Json;
+using System.Globalization;
+using OfficeOpenXml.Style;
+using System.Drawing;
 
 namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades.GarmentMasterPlan.MonitoringFacades
 {
@@ -38,6 +41,8 @@ namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades.GarmentMasterPlan
             var unitFilter = FilterDictionary.ContainsKey("unit") ? FilterDictionary["unit"] : "";
             var weeks = DbContext.GarmentWeeklyPlans.Include(a=>a.Items).Where(a=>a.Year==year);
             var data = Query.ToList();
+            var monthTemp = "";
+            int total = 0;
 
             if (!string.IsNullOrWhiteSpace(unitFilter))
             {
@@ -57,23 +62,102 @@ namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades.GarmentMasterPlan
                 if (!column.ContainsKey(d.UnitCode))
                 {
                     column.Add(unitCol, unitCol);
-                    result.Columns.Add(new DataColumn() { ColumnName = d.UnitCode, DataType = typeof(string) });
+                    result.Columns.Add(new DataColumn() { ColumnName = d.UnitCode, DataType = typeof(int) });
                 }
-                   
 
                 rowValuesForEmptyData.Add(0);
 
+                var month = "";
+                monthTemp = "";
                 foreach (var item in weeks.FirstOrDefault().Items.OrderBy(a=>a.WeekNumber))
                 {
-                    string week = string.Concat("W", item.WeekNumber);
+                    switch (item.EndDate.Month)
+                    {
+                        case 1:
+                            month = "Januari";
+                            break;
+                        case 2:
+                            month = "Februari";
+                            break;
+                        case 3:
+                            month = "Maret";
+                            break;
+                        case 4:
+                            month = "April";
+                            break;
+                        case 5:
+                            month = "Mei";
+                            break;
+                        case 6:
+                            month = "Juni";
+                            break;
+                        case 7:
+                            month = "Juli";
+                            break;
+                        case 8:
+                            month = "Agustus";
+                            break;
+                        case 9:
+                            month = "September";
+                            break;
+                        case 10:
+                            month = "Oktober";
+                            break;
+                        case 11:
+                            month = "November";
+                            break;
+                        case 12:
+                            month = "Desember";
+                            break;
+                        default:
+                            month = "";
+                            break;
+                    }
+
+                    string week = string.Concat("W", item.WeekNumber," - ", month);
 
                     var dataQty = data.FirstOrDefault(a => a.Unit.Equals(d.UnitCode) && a.WeekNumber.Equals(item.WeekNumber));
 
                     UnitDataTable unitDataTable = new UnitDataTable
                     {
                         Unit = d.UnitCode,
-                        qty= dataQty==null? "-" : dataQty.Quantity.ToString()
+                        qty= dataQty==null? 0 : (int)dataQty.Quantity
                     };
+
+                    //disini
+                    if (monthTemp == "")
+                    {
+                        monthTemp = month;
+                    }
+
+                    if (monthTemp == month)
+                    {
+                        total = dataQty==null? 0 + total : (int)dataQty.Quantity + total;
+                    } else if (monthTemp != month)
+                    {
+                        UnitDataTable unitDataTableTotal = new UnitDataTable
+                        {
+                            Unit = d.UnitCode,
+                            qty = total
+                        };
+                        string weekTotal = string.Concat("TOTAL ", monthTemp.ToUpper());
+                        if (rowData.ContainsKey(weekTotal))
+                        {
+                            var rowValue = rowData[weekTotal];
+                            var unit = rowValue.FirstOrDefault(a => a.Unit == d.UnitCode);
+                            if (unit == null)
+                            {
+                                rowValue.Add(unitDataTableTotal);
+                            }
+                        }
+                        else
+                        {
+                            rowData.Add(weekTotal, new List<UnitDataTable> { unitDataTableTotal });
+                        }
+                        total = dataQty == null ? 0 : (int)dataQty.Quantity;
+                        monthTemp = month;
+                    }
+
 
                     if (rowData.ContainsKey(week))
                     {
@@ -90,13 +174,34 @@ namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades.GarmentMasterPlan
                     }
                 }
 
+                UnitDataTable unitDataTableTotal2 = new UnitDataTable
+                {
+                    Unit = d.UnitCode,
+                    qty = total
+                };
+                string weekTotal2 = string.Concat("TOTAL ", monthTemp.ToUpper());
+                if (rowData.ContainsKey(weekTotal2))
+                {
+                    var rowValue = rowData[weekTotal2];
+                    var unit = rowValue.FirstOrDefault(a => a.Unit == d.UnitCode);
+                    if (unit == null)
+                    {
+                        rowValue.Add(unitDataTableTotal2);
+                    }
+                }
+                else
+                {
+                    rowData.Add(weekTotal2, new List<UnitDataTable> { unitDataTableTotal2 });
+                }
+                total = 0;
+
                 var sumQty = data.Where(a => a.Unit == d.UnitCode);
                 if (!rowData.ContainsKey("TOTAL"))
                 {
                     UnitDataTable unitDataTable1 = new UnitDataTable
                     {
                         Unit = d.UnitCode,
-                        qty = sumQty==null ? "-" : sumQty.Sum(a => a.Quantity).ToString() 
+                        qty = sumQty==null ? 0 : (int)sumQty.Sum(a => a.Quantity) 
                     };
                     rowData.Add("TOTAL", new List<UnitDataTable> { unitDataTable1 });
                 }
@@ -106,12 +211,11 @@ namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades.GarmentMasterPlan
                     UnitDataTable unitDataTable2 = new UnitDataTable
                     {
                         Unit = d.UnitCode,
-                        qty = sumQty == null ? "-" : sumQty.Sum(a => a.Quantity).ToString()
+                        qty = sumQty == null ? 0 : (int)sumQty.Sum(a => a.Quantity)
                     };
                     sumData.Add(unitDataTable2);
                 }
             }
-            
 
             if (data.Count == 0)
             {
@@ -129,13 +233,36 @@ namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades.GarmentMasterPlan
                     }
                     result.Rows.Add(rowValues.ToArray());
                 }
-
-                
             }
 
             ExcelPackage package = new ExcelPackage();
             var sheet = package.Workbook.Worksheets.Add("Monitoring Order Diterima");
             sheet.Cells["A1"].LoadFromDataTable(result, true, OfficeOpenXml.Table.TableStyles.Light16);
+
+            for (int y = 1; y <= sheet.Dimension.Rows; y++)
+            {
+                for (int x = 1; x <= sheet.Dimension.Columns; x++)
+                {
+                    var cell = sheet.Cells[y, x];
+
+                    if (y == 1)
+                    {
+                        cell.Style.Font.Bold = true;
+                    }
+                    else
+                    {
+                        if (sheet.Cells[y, x - x + 1].Value.ToString().Contains("TOTAL"))
+                        {
+                            cell.Style.Font.Bold = true;
+                        } else
+                        {
+                            cell.Style.Font.Bold = false;
+                        }
+                    }
+                }
+            }
+
+
             sheet.Cells[sheet.Dimension.Address].AutoFitColumns();
             MemoryStream streamExcel = new MemoryStream();
             package.SaveAs(streamExcel);
@@ -155,7 +282,7 @@ namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades.GarmentMasterPlan
         class UnitDataTable
         {
             public string Unit { get; set; }
-            public string qty { get; set; }
+            public int qty { get; set; }
         }
     }
 }
