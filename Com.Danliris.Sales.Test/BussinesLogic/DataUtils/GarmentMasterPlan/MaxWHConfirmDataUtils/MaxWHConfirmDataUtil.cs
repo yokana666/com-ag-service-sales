ecac1d89c3e200f07f74cab43e325ed4b64164df
achensetyawati <achenzetya@gmail.com>
2019-07-16 09:33:10 +0700

    MaxWHConfirm Tests

---
+++ Com.Danliris.Sales.Test/BussinesLogic/DataUtils/GarmentMasterPlan/MaxWHConfirmDataUtils/MaxWHConfirmDataUtil.cs
@@ -0,0 +1,26 @@
+﻿using Com.Danliris.Sales.Test.BussinesLogic.Utils;
+using Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades.GarmentMasterPlan.MaxWHConfirmFacades;
+using Com.Danliris.Service.Sales.Lib.Models.GarmentMasterPlan.MaxWHConfirmModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Sales.Test.BussinesLogic.DataUtils.GarmentMasterPlan.MaxWHConfirmDataUtils
+{
+    public class MaxWHConfirmDataUtil : BaseDataUtil<MaxWHConfirmFacade, MaxWHConfirm>
+    {
+        public MaxWHConfirmDataUtil(MaxWHConfirmFacade facade) : base(facade)
+        {
+        }
+
+        public override MaxWHConfirm GetNewData()
+        {
+            var wh = new MaxWHConfirm
+            {
+                MaxValue=70
+            };
+            
+            return wh;
+        }
+    }
+}
