1fcb725b2e684979c45fd057bde79ab507e0ce2d
achensetyawati <achenzetya@gmail.com>
2019-07-16 09:31:59 +0700

    Add MaxWHConfirm

---
+++ Com.Danliris.Service.Sales.Lib/AutoMapperProfiles/GarmentMasterPlanProfiles/MaxWHConfirmProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Com.Danliris.Service.Sales.Lib.Models.GarmentMasterPlan.MaxWHConfirmModel;
+using Com.Danliris.Service.Sales.Lib.ViewModels.GarmentMasterPlan.MaxWHConfirmViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Sales.Lib.AutoMapperProfiles.GarmentMasterPlanProfiles
+{
+    public class MaxWHConfirmProfile : Profile
+    {
+        public MaxWHConfirmProfile()
+        {
+            CreateMap<MaxWHConfirm, MaxWHConfirmViewModel>()
+                .ReverseMap();
+        }
+    }
+}
---
+++ Com.Danliris.Service.Sales.Lib/ViewModels/GarmentMasterPlan/MaxWHConfirmViewModels/MaxWHConfirmViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Com.Danliris.Service.Sales.Lib.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Com.Danliris.Service.Sales.Lib.ViewModels.GarmentMasterPlan.MaxWHConfirmViewModels
+{
+    public class MaxWHConfirmViewModel : BaseViewModel, IValidatableObject
+    {
+        public double MaxValue { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (MaxValue <=0)
+            {
+                yield return new ValidationResult("Max WH Confirm tidak boleh kosong", new List<string> { "wh" });
+            }
+        }
+    }
+}
