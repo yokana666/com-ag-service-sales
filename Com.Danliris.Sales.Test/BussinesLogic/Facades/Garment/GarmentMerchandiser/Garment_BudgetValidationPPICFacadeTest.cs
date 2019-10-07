d417beab5c2ea435b9dbad22c4eb21ecf6c79ed3
kuswandanu <kuswandanu@gmail.com>
2019-10-07 15:01:58 +0700

    Update Garment_BudgetValidationPPICFacadeTest.cs

--- Com.Danliris.Sales.Test/BussinesLogic/Facades/Garment/GarmentMerchandiser/Garment_BudgetValidationPPICFacadeTest.cs
+++ Com.Danliris.Sales.Test/BussinesLogic/Facades/Garment/GarmentMerchandiser/Garment_BudgetValidationPPICFacadeTest.cs
@@ -18,6 +18,8 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Text;
 using Xunit;
@@ -102,11 +104,25 @@ namespace Com.Danliris.Sales.Test.BussinesLogic.Facades.Garment.GarmentMerchandi
         public async void Validate_RO_Garment_Success()
         {
             var dbContext = DbContext(GetCurrentMethod());
-            var serviceProvider = GetServiceProviderMock(dbContext).Object;
+            var serviceProviderMock = GetServiceProviderMock(dbContext);
+
+            Mock<IHttpClientService> httpClientMock = new Mock<IHttpClientService>();
+
+            httpClientMock.Setup(x => x.PostAsync(It.IsAny<string>(), It.IsAny<HttpContent>()))
+                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.Created));
+
+            serviceProviderMock.Setup(x => x.GetService(typeof(IHttpClientService)))
+                .Returns(httpClientMock.Object);
+
+            var serviceProvider = serviceProviderMock.Object;
 
             CostCalculationGarmentFacade facade = new CostCalculationGarmentFacade(serviceProvider, dbContext);
 
             var dataCostCalculationGarment = await DataUtil(facade, serviceProvider, dbContext).GetTestData();
+            foreach (var material in dataCostCalculationGarment.CostCalculationGarment_Materials)
+            {
+                material.IsPRMaster = false;
+            }
 
             Garment_BudgetValidationPPICFacade garmentValidationFacade = new Garment_BudgetValidationPPICFacade(serviceProvider, dbContext);
 
@@ -141,10 +157,50 @@ namespace Com.Danliris.Sales.Test.BussinesLogic.Facades.Garment.GarmentMerchandi
         }
 
         [Fact]
+        public async void Validate_RO_Garment_Success_Category_PROCESS()
+        {
+            var dbContext = DbContext(GetCurrentMethod());
+            var serviceProviderMock = GetServiceProviderMock(dbContext);
+
+            Mock<IHttpClientService> httpClientMock = new Mock<IHttpClientService>();
+
+            httpClientMock.Setup(x => x.GetAsync(It.IsAny<string>()))
+                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("{ data: { Items: [] } }", Encoding.UTF8, General.JsonMediaType) });
+            httpClientMock.Setup(x => x.PutAsync(It.IsAny<string>(), It.IsAny<HttpContent>()))
+                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.Created));
+
+            serviceProviderMock.Setup(x => x.GetService(typeof(IHttpClientService)))
+                .Returns(httpClientMock.Object);
+
+            var serviceProvider = serviceProviderMock.Object;
+
+            CostCalculationGarmentFacade facade = new CostCalculationGarmentFacade(serviceProvider, dbContext);
+
+            var dataCostCalculationGarment = await DataUtil(facade, serviceProvider, dbContext).GetTestData();
+            foreach (var material in dataCostCalculationGarment.CostCalculationGarment_Materials)
+            {
+                material.CategoryName = "PROCESS";
+                material.IsPRMaster = false;
+            }
+
+            Garment_BudgetValidationPPICFacade garmentValidationFacade = new Garment_BudgetValidationPPICFacade(serviceProvider, dbContext);
+
+            var productDict = dataCostCalculationGarment.CostCalculationGarment_Materials.ToDictionary(k => long.Parse(k.ProductId), v => v.ProductCode);
+
+            var result = await garmentValidationFacade.ValidateROGarment(dataCostCalculationGarment, productDict);
+
+            Assert.NotEqual(0, result);
+        }
+
+        [Fact]
         public async void Validate_RO_Garment_Error_Category_PROCESS()
         {
             var dbContext = DbContext(GetCurrentMethod());
-            var serviceProvider = GetServiceProviderMock(dbContext).Object;
+            var serviceProviderMock = GetServiceProviderMock(dbContext);
+
+            //serviceProviderMock.Setup()
+
+            var serviceProvider = serviceProviderMock.Object;
 
             CostCalculationGarmentFacade facade = new CostCalculationGarmentFacade(serviceProvider, dbContext);
 
