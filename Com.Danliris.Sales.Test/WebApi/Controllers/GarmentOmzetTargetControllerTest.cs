493e9488b235e7db29bcefd0e00b06024249513c
ANDRI PURNOMO <andrea.kinan@gmail.com>
2019-10-24 16:41:02 +0700

    Add Service & Create Table | Garment Omzet Target
    Add Service, Unit Test & Create Table | Garment Omzet Target

---
+++ Com.Danliris.Sales.Test/WebApi/Controllers/GarmentOmzetTargetControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Com.Danliris.Sales.Test.WebApi.Utils;
+using Com.Danliris.Service.Sales.Lib.BusinessLogic.Interface.GarmentOmzetTargetInterface;
+using Com.Danliris.Service.Sales.Lib.Models.GarmentOmzetTargetModel;
+using Com.Danliris.Service.Sales.Lib.Services;
+using Com.Danliris.Service.Sales.Lib.Utilities;
+using Com.Danliris.Service.Sales.Lib.ViewModels.CostCalculationGarment;
+using Com.Danliris.Service.Sales.Lib.ViewModels.GarmentOmzetTargetViewModels;
+using Com.Danliris.Service.Sales.WebApi.Controllers;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Com.Danliris.Sales.Test.WebApi.Controllers
+{
+    public class GarmentOmzetTargetControllerTest : BaseControllerTest<GarmentOmzetTargetController, GarmentOmzetTarget, GarmentOmzetTargetViewModel, IGarmentOmzetTarget>
+    {        
+    }
+}
---
+++ Com.Danliris.Service.Sales.Lib/BusinessLogic/Interface/GarmentOmzetTargetInterface/IGarmentOmzetTarget.cs
@@ -0,0 +1,15 @@
+﻿using Com.Danliris.Service.Sales.Lib.Models.GarmentOmzetTargetModel;
+using Com.Danliris.Service.Sales.Lib.Utilities.BaseInterface;
+using Com.Danliris.Service.Sales.Lib.ViewModels.GarmentOmzetTargetViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Interface.GarmentOmzetTargetInterface
+{
+    public interface IGarmentOmzetTarget : IBaseFacade<GarmentOmzetTarget>
+    {
+     
+    }
+}
