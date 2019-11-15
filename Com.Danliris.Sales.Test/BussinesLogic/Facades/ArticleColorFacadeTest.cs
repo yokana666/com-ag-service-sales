00313f7f93edb18901de909664e34565151c6fe4
ali-moonlay <43127426+ali-moonlay@users.noreply.github.com>
2019-11-15 17:59:24 +0700

    Enhance Unit Test

---
+++ Com.Danliris.Sales.Test/BussinesLogic/Facades/ArticleColorFacadeTest.cs
@@ -0,0 +1,39 @@
+﻿using Com.Danliris.Sales.Test.BussinesLogic.DataUtils;
+using Com.Danliris.Sales.Test.BussinesLogic.Utils;
+using Com.Danliris.Service.Sales.Lib;
+using Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades;
+using Com.Danliris.Service.Sales.Lib.BusinessLogic.Logic;
+using Com.Danliris.Service.Sales.Lib.Models;
+using Com.Danliris.Service.Sales.Lib.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Sales.Test.BussinesLogic.Facades
+{
+    public class ArticleColorFacadeTest : BaseFacadeTest<SalesDbContext, ArticleColorFacade, ArticleColorLogic, ArticleColor, ArticleColorDataUtil>
+    {
+        private const string ENTITY = "ArticleColor";
+        public ArticleColorFacadeTest() : base(ENTITY)
+        {
+        }
+
+        protected override Mock<IServiceProvider> GetServiceProviderMock(SalesDbContext dbContext)
+        {
+            var serviceProviderMock = new Mock<IServiceProvider>();
+
+            IIdentityService identityService = new IdentityService { Username = "Username" };
+
+            serviceProviderMock
+                .Setup(x => x.GetService(typeof(IdentityService)))
+                .Returns(identityService);
+
+            serviceProviderMock
+                .Setup(x => x.GetService(typeof(ArticleColorLogic)))
+                .Returns(Activator.CreateInstance(typeof(ArticleColorLogic), serviceProviderMock.Object, identityService, dbContext) as ArticleColorLogic);
+
+            return serviceProviderMock;
+        }
+    }
+}
