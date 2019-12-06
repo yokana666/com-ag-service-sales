d211a2a784d70a2516a76b9dbafff17549da6a7c
ali-moonlay <43127426+ali-moonlay@users.noreply.github.com>
2019-12-06 14:10:16 +0700

    Update FinishingPrintingCostCalculationFacadeTest.cs

--- Com.Danliris.Sales.Test/BussinesLogic/Facades/FinishingPrintingCostCalculation/FinishingPrintingCostCalculationFacadeTest.cs
+++ Com.Danliris.Sales.Test/BussinesLogic/Facades/FinishingPrintingCostCalculation/FinishingPrintingCostCalculationFacadeTest.cs
@@ -4,19 +4,28 @@ using Com.Danliris.Service.Sales.Lib;
 using Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades.FinishingPrintingCostCalculation;
 using Com.Danliris.Service.Sales.Lib.BusinessLogic.Logic.FinishingPrintingCostCalculation;
 using Com.Danliris.Service.Sales.Lib.Models.FinishingPrintingCostCalculation;
+using Com.Danliris.Service.Sales.Lib.ViewModels.FinishingPrintingCostCalculation;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace Com.Danliris.Sales.Test.BussinesLogic.Facades.FinishingPrintingCostCalculation
 {
     public class FinishingPrintingCostCalculationFacadeTest : BaseFacadeTest<SalesDbContext, FinishingPrintingCostCalculationFacade, FinishingPrintingCostCalculationLogic, FinishingPrintingCostCalculationModel, FinishingPrintingCostCalculationDataUtils>
     {
         private const string ENTITY = "FinishingPrintingCostCalculation";
-        
+
         public FinishingPrintingCostCalculationFacadeTest() : base(ENTITY)
         {
         }
 
+        [Fact]
+        public void ValidateVM()
+        {
+            var vm = new FinishingPrintingCostCalculationViewModel();
+            var response = vm.Validate(null);
+            Assert.NotEmpty(response);
+        }
     }
 }
