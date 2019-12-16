7c8c413ca238c201604648cfd19f1ecf822b8952
Franciskus Napitupulu <8franciskusna70@gmail.com>
2019-12-16 14:21:18 +0700

    Revert "Revert "Sales Invoice""
    This reverts commit 26bb93f484a1d951c0f0a017881d7954dc19a07f.

---
+++ Com.Danliris.Service.Sales.Lib/BusinessLogic/Logic/SalesInvoice/SalesInvoiceDetailLogic.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Com.Danliris.Service.Sales.Lib.Models.SalesInvoice;
+using Com.Danliris.Service.Sales.Lib.Services;
+using Com.Danliris.Service.Sales.Lib.Utilities;
+using Com.Danliris.Service.Sales.Lib.Utilities.BaseClass;
+using Com.Moonlay.NetCore.Lib;
+using Newtonsoft.Json;
+
+namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Logic.SalesInvoice
+{
+    public class SalesInvoiceDetailLogic : BaseLogic<SalesInvoiceDetailModel>
+    {
+        public SalesInvoiceDetailLogic(IServiceProvider serviceProvider, IIdentityService identityService, SalesDbContext dbContext) : base(identityService, serviceProvider, dbContext)
+        {
+        }
+
+        public override ReadResponse<SalesInvoiceDetailModel> Read(int page, int size, string order, List<string> select, string keyword, string filter)
+        {
+            IQueryable<SalesInvoiceDetailModel> Query = DbSet;
+
+            List<string> SearchAttributes = new List<string>()
+            {
+              ""
+            };
+
+            Query = QueryHelper<SalesInvoiceDetailModel>.Search(Query, SearchAttributes, keyword);
+
+            Dictionary<string, object> FilterDictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(filter);
+            Query = QueryHelper<SalesInvoiceDetailModel>.Filter(Query, FilterDictionary);
+
+            List<string> SelectedFields = new List<string>()
+            {
+                "Id","Quantity","Total","UomId","UomUnit","UnitCode","UnitName","UnitPrice","Amount","LastModifiedUtc"
+            };
+
+            Query = Query
+                .Select(field => new SalesInvoiceDetailModel
+                {
+                    Id = field.Id,
+                    Quantity = field.Quantity,
+                    Total = field.Total,
+                    UomId = field.UomId,
+                    UomUnit = field.UomUnit,
+                    UnitCode = field.UnitCode,
+                    UnitName = field.UnitName,
+                    UnitPrice = field.UnitPrice,
+                    Amount = field.Amount,
+                    LastModifiedUtc = field.LastModifiedUtc,
+                });
+
+            Dictionary<string, string> OrderDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(order);
+            Query = QueryHelper<SalesInvoiceDetailModel>.Order(Query, OrderDictionary);
+
+            Pageable<SalesInvoiceDetailModel> pageable = new Pageable<SalesInvoiceDetailModel>(Query, page - 1, size);
+            List<SalesInvoiceDetailModel> data = pageable.Data.ToList<SalesInvoiceDetailModel>();
+            int totalData = pageable.TotalCount;
+
+            return new ReadResponse<SalesInvoiceDetailModel>(data, totalData, OrderDictionary, SelectedFields);
+        }
+        public HashSet<long> GetIds(long id)
+        {
+            return new HashSet<long>(DbSet.Where(d => d.SalesInvoiceModel.Id == id).Select(d => d.Id));
+        }
+    }
+}
--- Com.Danliris.Service.Sales.Lib/Migrations/20191211040054_editEntityCurrency.Designer.cs
+++ Com.Danliris.Service.Sales.Lib/Migrations/20191211040054_editEntityCurrency.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Sales.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Sales.Lib.Migrations
 {
     [DbContext(typeof(SalesDbContext))]
-    partial class SalesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191211040054_editEntityCurrency")]
+    partial class editEntityCurrency
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -2556,9 +2558,6 @@ namespace Com.Danliris.Service.Sales.Lib.Migrations
                     b.Property<string>("DeliveryOrderNo")
                         .HasMaxLength(25);
 
-                    b.Property<string>("Disp")
-                        .HasMaxLength(25);
-
                     b.Property<DateTimeOffset>("DueDate");
 
                     b.Property<bool>("IsDeleted");
@@ -2582,15 +2581,9 @@ namespace Com.Danliris.Service.Sales.Lib.Migrations
                     b.Property<string>("Notes")
                         .HasMaxLength(500);
 
-                    b.Property<string>("Op")
-                        .HasMaxLength(25);
-
                     b.Property<DateTimeOffset>("SalesInvoiceDate");
 
                     b.Property<string>("SalesInvoiceNo")
-                        .HasMaxLength(25);
-
-                    b.Property<string>("Sc")
                         .HasMaxLength(25);
 
                     b.Property<string>("UId")
