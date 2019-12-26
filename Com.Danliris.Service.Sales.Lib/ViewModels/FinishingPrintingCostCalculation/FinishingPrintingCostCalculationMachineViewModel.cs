89b5dc02d54c069fde3ceec074c92e322a40af1f
Franciskus Napitupulu <8franciskusna70@gmail.com>
2019-12-26 18:53:20 +0700

    FixingMissingFile

---
+++ Com.Danliris.Service.Sales.Lib/ViewModels/FinishingPrintingCostCalculation/FinishingPrintingCostCalculationMachineViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Com.Danliris.Service.Sales.Lib.Models.FinishingPrintingCostCalculation;
+using Com.Danliris.Service.Sales.Lib.Utilities;
+using Com.Danliris.Service.Sales.Lib.ViewModels.IntegrationViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Com.Danliris.Service.Sales.Lib.ViewModels.FinishingPrintingCostCalculation
+{
+    public class FinishingPrintingCostCalculationMachineViewModel : BaseViewModel
+    {
+        public StepViewModel Step { get; set; }
+        public long CostCalculationId { get; set; }
+        public MachineViewModel Machine { get; set; }
+        public decimal Depretiation { get; set; }
+        public List<FinishingPrintingCostCalculationChemicalViewModel> Chemicals { get; set; }
+        public int Index { get; set; }
+        
+    }
+}
