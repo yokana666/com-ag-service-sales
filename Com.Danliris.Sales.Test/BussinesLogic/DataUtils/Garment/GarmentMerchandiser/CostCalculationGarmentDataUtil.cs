fb13c495d4551d80d2240b5fd5ad63b4ecd181ad
ANDRI PURNOMO <andrea.kinan@gmail.com>
2020-02-05 14:23:33 +0700

    Update Logic | CC Garment Data Utils
    Update Logic | CC Garment Data Utils

--- Com.Danliris.Sales.Test/BussinesLogic/DataUtils/Garment/GarmentMerchandiser/CostCalculationGarmentDataUtil.cs
+++ Com.Danliris.Sales.Test/BussinesLogic/DataUtils/Garment/GarmentMerchandiser/CostCalculationGarmentDataUtil.cs
@@ -74,6 +74,7 @@ namespace Com.Danliris.Sales.Test.BussinesLogic.DataUtils.Garment.GarmentMerchan
             data.IsApprovedIE = false;
             data.IsApprovedPurchasing = false;
             data.IsApprovedKadivMD = true;
+            data.IsValidatedROSample = true;
             data.ApprovedKadivMDBy = "Test";
             data.ApprovedKadivMDDate = DateTimeOffset.Now;
             data.ValidationSampleDate = DateTimeOffset.Now;
